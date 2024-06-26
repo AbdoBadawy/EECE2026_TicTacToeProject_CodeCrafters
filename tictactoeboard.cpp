af9c1511c1f68695a60302ff57ff4d9d30950faa
Abdulrahman Badawy <Abdulrahman.Taher03@eng-st.cu.edu.eg>
2024-06-26 20:36:06 +0300

    AI updated

--- tictactoeboard.cpp
+++ tictactoeboard.cpp
@@ -53,8 +53,6 @@ void TicTacToeBoard::handleButtonClick(QPushButton *button, int row, int col) {
             board_buffer.push_back(ai_mod);
             board_buffer.push_back(player1Score);
             board_buffer.push_back(player2Score);
-            for(auto it=board_buffer.begin();it<board_buffer.end();it++)
-                std::cout << *it << endl;
             history_insert(username,board_buffer);
             justReset = false; //To indicate this a valid game
 
@@ -71,8 +69,6 @@ void TicTacToeBoard::handleButtonClick(QPushButton *button, int row, int col) {
             board_buffer.push_back(ai_mod);
             board_buffer.push_back(player1Score);
             board_buffer.push_back(player2Score);
-            for(auto it=board_buffer.begin();it<board_buffer.end();it++)
-                std::cout << *it << endl;
             history_insert(username,board_buffer);
             justReset = false; //To indicate this a valid game
 
@@ -180,114 +176,116 @@ int TicTacToeBoard::evaluate()
 // Else if none of them have won then return 0
 return 0;
 }
-int TicTacToeBoard::minimax(bool isMax,int depth)
+int TicTacToeBoard::minimax(bool isMax,int depth,int alpha,int beta)
 {
-   int score = evaluate();
+    int score = evaluate();
 
-   // If Maximizer has won the game return his/her
-   // evaluated score
-   if (score == 20)
-           return score;
+    // If Maximizer has won the game return his/her
+    // evaluated score
+    if (score == 20)
+        return score-depth;
 
-   // If Minimizer has won the game return his/her
-   // evaluated score
-   if (score == -20)
-           return score;
+    // If Minimizer has won the game return his/her
+    // evaluated score
+    if (score == -20)
+        return depth+score;
 
-   // If there are no more moves and no winner then
-   // it is a tie
-   if (checkDraw()==true)
-           return 0;
+    // If there are no more moves and no winner then
+    // it is a tie
+    if (checkDraw()==true)
+        return 0;
 
-   // If this maximizer's move
-   if (isMax)
-   {
-       int best = -1000;
+    // If this maximizer's move
+    if (isMax)
+    {
+        int best = -1000;
 
-       // Traverse all cells
-       for (int i = 0; i<3; i++){
-           for (int j = 0; j<3; j++){
+        // Traverse all cells
+        for (int i = 0; i<3; i++){
+            for (int j = 0; j<3; j++){
 
-           // Check if cell is empty
-               if (board[i][j]==0){
+                // Check if cell is empty
+                if (board[i][j]==0){
 
-                   // Make the move
-                   board[i][j] = 2;
+                    // Make the move
+                    board[i][j] = 2;
+                    int eval = minimax( false, depth + 1, alpha, beta);
+                        // Undo the move
+                    board[i][j] = 0;
+                    best = std::max( best,eval);
 
-                   // Call minimax recursively and choose
-                   // the maximum value
-                   best = std::max( best,minimax(!isMax,depth+1));
 
-                   // Undo the move
-                   board[i][j] = 0;
+                    alpha = std::max(alpha, eval);
+                    if (beta <= alpha) break;
+                }
+            }
+        }
+        return best;
+    }
 
-               }
-           }
-       }
-       return best+depth;
-   }
+    // If this minimizer's move
+    else{
+        int best = 1000;
 
-   // If this minimizer's move
-   else{
-       int best = 1000;
+        // Traverse all cells
+        for (int i = 0; i<3; i++){
+            for (int j = 0; j<3; j++){
 
-       // Traverse all cells
-       for (int i = 0; i<3; i++){
-           for (int j = 0; j<3; j++){
+                // Check if cell is empty
+                if (board[i][j]==0){
 
-               // Check if cell is empty
-               if (board[i][j]==0){
+                    // Make the move
+                    board[i][j] = 1;
+                    int eval = minimax( true, depth + 1, alpha, beta);
+                    // Undo the move
+                    board[i][j] = 0;
+                    // Call minimax recursively and choose
+                    // the minimum value
+                    best = std::min(best,eval);
+                    beta = std::min(beta, eval);
+                    if (beta <= alpha) break;
 
-                   // Make the move
-                   board[i][j] = 1;
 
-                   // Call minimax recursively and choose
-                   // the minimum value
-                   best = std::min(best,
-                          minimax(!isMax,depth+1));
-
-                   // Undo the move
-                   board[i][j] = 0;
-               }
-           }
-       }
-       return best-depth;
-   }
+                }
+            }
+        }
+        return best;
+    }
 }
 int TicTacToeBoard::findBestMove(){
-   int bestVal = -1000;
-   int bestMove;
+    int bestVal = -1000;
+    int bestMove;
 
-   // Traverse all cells, evaluate minimax function for
-   // all empty cells. And return the cell with optimal
-   // value.
-   for (int i = 0; i<3; i++){
-       for (int j = 0; j<3; j++){
+    // Traverse all cells, evaluate minimax function for
+    // all empty cells. And return the cell with optimal
+    // value.
+    for (int row = 0; row<3; row++){
+        for (int col = 0; col<3; col++){
 
-           // Check if cell is empty
-           if (board[i][j]==0){
+            // Check if cell is empty
+            if (board[row][col]==0){
 
-               // Make the move
-               board[i][j] = 2;
+                // Make the move
+                board[row][col] = 2;
 
-               // compute evaluation function for this
-               // move.
-               int moveVal = minimax(false,0);
+                // compute evaluation function for this
+                // move.
+                int moveVal = minimax(false,0,-1000,1000);
 
-               // Undo the move
-               board[i][j] = 0;
+                // Undo the move
+                board[row][col] = 0;
 
-               // If the value of the current move is
-               // more than the best value, then update
-               // best/
-               if (moveVal > bestVal){
-                   bestMove = 3*i+j;
-                   bestVal = moveVal;
-               }
-           }
-       }
-   }
-   return bestMove;
+                // If the value of the current move is
+                // more than the best value, then update
+                // best/
+                if (moveVal > bestVal){
+                    bestMove = 3*row+col;
+                    bestVal = moveVal;
+                }
+            }
+        }
+    }
+    return bestMove;
 }
 
 
